0bd04e7e37be82a7f82eea7ec882cc120302ed7d
minh <mreigen@gmail.com>
2014-09-14 01:07:21 -0700

    load all files in main cms_api.rb

---
+++ lib/cms_api.rb
@@ -0,0 +1,11 @@
+require_relative "cms_api/setup.rb"
+require_relative "cms_api/request.rb"
+require_relative "cms_api/response.rb"
+require_relative "cms_api/base_object.rb"
+require_relative "cms_api/content_item.rb"
+require_relative "cms_api/label.rb"
+require_relative "cms_api/tenant.rb"
+require_relative "cms_api/uploader.rb"
+
+class CmsApi
+end
